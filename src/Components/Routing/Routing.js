5f1bf6345cfca0b6564d632653121affb00e8d68
Suvosg1998 <subhajitg1234567@gmail.com>
2024-09-23 10:06:54 +0530

    newly created routing folder

---
+++ src/Components/Routing/Routing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Route,Routes,BrowserRouter } from 'react-router-dom'
+const Routing = () => {
+  return (
+    <div>Routing</div>
+  )
+}
+
+export default Routing
